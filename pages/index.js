47905e20fe643eb5abaed94b28dd92e7ed2c6548
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-29 19:36:36 +0100

    created a dummy data and output the list of meetups

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some city",
+    description: "This is the first meeting",
+  },
+  {
+    id: "m2",
+    title: "The Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some city",
+    description: "This is the second meeting",
+  },
+  {
+    id: "m3",
+    title: "The Third Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 15, 12345 Some city",
+    description: "This is the third meeting",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
