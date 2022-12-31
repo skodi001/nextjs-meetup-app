3d45d8a08305b4126aceb31434bd439667560fa3
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-31 09:41:28 +0100

    Working with MongoDB and updated [meetup] as well

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,4 @@
-import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
-import { useRouter } from "next/router";
 
 function MeetupDetails() {
   return (
@@ -8,12 +6,12 @@ function MeetupDetails() {
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
       title="First Meetup"
       address="Some Street 5, Some City"
-      description="This is the first meetup"
+      description="This is a first meetup"
     />
   );
 }
 
-export async function getStaticPath() {
+export async function getStaticPaths() {
   return {
     fallback: false,
     paths: [
@@ -32,7 +30,10 @@ export async function getStaticPath() {
 }
 
 export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
   const meetupId = context.params.meetupId;
+
   console.log(meetupId);
 
   return {
@@ -43,7 +44,7 @@ export async function getStaticProps(context) {
         id: meetupId,
         title: "First Meetup",
         address: "Some Street 5, Some City",
-        description: "This is the first meetup",
+        description: "This is a first meetup",
       },
     },
   };
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,25 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://skodi001:Shoh19981009!@cluster0.rhpuu11.mongodb.net/test"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.satus(201).json({ message: "Meetups updated successfully" });
   }
 }
 
